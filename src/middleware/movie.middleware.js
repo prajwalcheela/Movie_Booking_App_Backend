641acb1c50eae667f95c70810710d151666f31f6
prajwalcheela <66377842+prajwalcheela@users.noreply.github.com>
2024-02-04 19:09:38 +0530

    rectified error in Movie middleware

--- src/middleware/movie.middleware.js
+++ src/middleware/movie.middleware.js
@@ -1,6 +1,14 @@
 exports.MveVerify=(req,res,next)=>{
-    const {name,description,casts,trailUrl,posterUrl,language,director}=req.body
-    if(!name||!description||!casts||!trailUrl||!posterUrl||!language||!director){
+    const {name,description,casts,trailerUrl,posterUrl,language,director}=req.body
+    // console.log(name)
+    // console.log(director)
+    // console.log(description)
+    // console.log(casts)
+    // console.log(trailerUrl)
+    // console.log(posterUrl)
+    // console.log(language)
+    if(!name||!description||!casts||!trailerUrl||!posterUrl||!language||!director){
+       
         return res.status(400).json({message:"name,description,casts,trailUrl,posterUrl,language,director are required fields"})
 
     }
